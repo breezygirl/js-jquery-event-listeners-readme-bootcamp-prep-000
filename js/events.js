65f7dd9ca170e260719e048746cf4abe356aba6f
Beerscheba C <designergo@aol.com>
2018-07-12 18:49:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,16 @@
 //define functions here
+function getIt(){
+  
+  alert("Hey!")
+}
 
+function frameIt(){
+ $('img').on('load', function(){
+  //actions you want to happen
+}); 
+
+  
+}
 $(document).ready(function(){
 
 // call functions here
